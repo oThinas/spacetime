894ae768a7ce8661ecdffc022c09983027363d56
oThinas <thiagomartinsprado@gmail.com>
2023-05-25 13:46:51 -0300

    feat: cors para todas as origens

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,7 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
 
 const app = fastify();
-const prisma = new PrismaClient();
-
-app.get('/users', async (__, response) => {
-  const users = await prisma.user.findMany();
-  return response.send(users);
-});
+app.register(fastifyCors, { origin: true });
 
 app.listen({ port: 3333, host: 'localhost' }).then(() => console.log('⌛ Servidor rodando em http://localhost:3333'));
