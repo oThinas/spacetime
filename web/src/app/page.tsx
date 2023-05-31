673576a63650efe66a9840fb6a0aedfa6eb8591c
oThinas <thiagomartinsprado@gmail.com>
2023-05-31 14:57:23 -0300

    feat: página home componentizada

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,4 @@
-import { User } from 'lucide-react';
-
-import logo from '../assets/logo.svg';
-import Image from 'next/image';
+import { Copyright, EmptyMemories, Hero, SignIn } from '@/components';
 
 export default function Home() {
   return (
@@ -20,65 +17,18 @@ export default function Home() {
         <div className='absolute inset-y-0 right-2 w-2 bg-stripes'/>
 
         {/* Sign in */}
-        <a href='' className='group flex items-center gap-3 text-left transition-colors hover:text-gray-50'>
-          <div className='flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 transition-transform group-hover:scale-125'>
-            <User className='h-5 w-5 text-gray-500 transition-colors group-hover:text-gray-50'/>
-          </div>
-
-          <p className='max-w-[140px] text-sm leading-snug'>
-            <span className='underline'>
-              Crie sua conta
-            </span>
-            {' '} e salve suas memórias
-          </p>
-        </a>
+        <SignIn/>
 
         {/* Hero */}
-        <div className='space-y-5'>
-          <Image src={logo} alt='Logo da Spacetime'/>
-
-          <div className='max-w-[420px] space-y-1'>
-            <h1 className='text-5xl font-bold leading-tight text-gray-50'>
-              Sua cápsula do tempo
-            </h1>
-
-            <p className='text-lg leading-relaxed'>
-              Colecione momentos marcantes da sua jornada e compartilhe com o mundo!
-            </p>
-          </div>
-
-          <a href='' className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600'>
-            Cadastrar lembrança
-          </a>
-        </div>
+        <Hero/>
 
         {/* Footer */}
-        <div className='text-sm leading-relaxed text-gray-200'>
-          <p>
-            Feito com 💜 no NLW da{' '}
-            <a
-              href='https://rocketseat.com.br'
-              target='_blank'
-              rel='noreferrer'
-              className='underline hover:text-gray-100'
-            >
-              Rocketseat
-            </a>
-          </p>
-        </div>
+        <Copyright/>
       </div>
 
       {/* Right panel */}
       <div className='flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16'>
-        <div className='flex flex-1 items-center justify-center'>
-          <p className='w-[360px] text-center leading-relaxed'>
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href='' className='underline hover:text-gray-50'>
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories/>
       </div>
     </main>
   );
---
+++ web/src/components/index.ts
@@ -0,0 +1,4 @@
+export { Copyright } from './Copyright';
+export { EmptyMemories } from './EmptyMemories';
+export { Hero } from './Hero';
+export { SignIn } from './SignIn';
